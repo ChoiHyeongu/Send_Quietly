bb49b0edf96586fc00daa2da7f53f1e022b14832
ChoiHyeongu <a11dsd123@gmail.com>
2019-10-29 21:40:47 +0900

    Recycler view

--- app/src/main/java/com/motivation/sendquiet/activity/LoginActivity.java
+++ app/src/main/java/com/motivation/sendquiet/activity/LoginActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,8 +16,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.motivation.sendquiet.R;
 import com.motivation.sendquiet.model.User;
-
-import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/motivation/sendquiet/activity/MemberFragment.java
+++ app/src/main/java/com/motivation/sendquiet/activity/MemberFragment.java
@@ -1,21 +1,40 @@
 package com.motivation.sendquiet.activity;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.motivation.sendquiet.R;
+import com.motivation.sendquiet.adapter.MemberListAdapter;
 import com.motivation.sendquiet.model.User;
+
+import java.util.ArrayList;
 
 public class MemberFragment extends Fragment {
 
     final String TAG = "MemberFragment";
 
-    User user;
+    RecyclerView memberListView;
+
+    private User user;
+    private ArrayList<User> users = new ArrayList<>();
+    private MemberListAdapter memberListAdapter;
+
+    //Firebase
+    private DatabaseReference userReference;
+    private DatabaseReference reference;
 
     public MemberFragment() {
         // Required empty public constructor
@@ -35,9 +54,42 @@ public class MemberFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_member, container, false);
+        memberListView = view.findViewById(R.id.member_list);
+
+        userReference = FirebaseDatabase.getInstance().getReference("User");
+        users = setUserList();
+
+        for(int i = 0; i < users.size(); i++){
+            Log.d("onCV", users.get(i).getName());
+        }
+
+        memberListView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         return view;
     }
 
+    private ArrayList<User> setUserList(){
 
+        ArrayList<User> list = new ArrayList<>();
+
+        userReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                users.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    list.add(user);
+                    Log.d("onDataChange", user.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        Log.d("setUserLIst", String.valueOf(list.size()));
+        return list;
+    }
 }
